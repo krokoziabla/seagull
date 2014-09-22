77b086e9eabe84caf50902bf72b18a93ef0d7107
Vitaly Kirsanov <vkirsan@mera.ru>
2014-09-22 18:51:17 +0400

    System stack pointer in .data
    I've allocated 4KiB for the system stack. I hope that will be enough.

--- src/vectors.s
+++ src/vectors.s
@@ -1,6 +1,6 @@
 .section vectors, "a"
 
-.long 0 //.long __startup_stack             // 0 - Main stack pointer
+.long __system_stack            // 0 - Main stack pointer
 .long __reset_vector            // 1 - Reset
 .long 0
 .long __hard_fault_vector       // 3 - Hard Fault
