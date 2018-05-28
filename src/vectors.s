c0a4b22d88f5c5e8e9292caaed8bd92936432392
Vitaly Kirsanov <krokoziabla@intel.com>
2018-05-28 10:22:27 +0300

    Vectors moved to the linker script

--- src/vectors.s
+++
@@ -1,6 +0,0 @@
-.section vectors, "a"
-
-.long __system_stack            // 0 - Main stack pointer
-.long __reset_vector            // 1 - Reset
-.long 0
-.long __hard_fault_vector       // 3 - Hard Fault
